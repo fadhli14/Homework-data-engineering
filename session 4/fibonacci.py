4c0930a096fba316b0b6dcb84e3f79e76bd10113
fadhli14 <81307461+fadhli14@users.noreply.github.com>
2021-03-28 23:31:20 +0700

    Create fibonacci.py

---
+++ session 4/fibonacci.py
@@ -0,0 +1,11 @@
+banyak_bilangan = 4
+count = 0
+n1 = 2
+n2 = 3
+ 
+while count < banyak_bilangan:
+   print(n1)
+   bilangan_terakhir = n1 + n2
+   n1 = n2
+   n2 = bilangan_terakhir
+   count += 1
